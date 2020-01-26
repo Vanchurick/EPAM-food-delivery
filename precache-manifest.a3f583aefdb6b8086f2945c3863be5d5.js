3e78281f1ab11f45c9ad792f55f0e89028534a2d
Vanchurick <davane4ek@gmail.com>
2020-01-26 12:45:27 +0200

    Updates

---
+++ precache-manifest.a3f583aefdb6b8086f2945c3863be5d5.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4ad59dc59622b6f5f7c13c1fe3864c",
+    "url": "/EPAM-food-delivery/index.html"
+  },
+  {
+    "revision": "8d49be821a6138055073",
+    "url": "/EPAM-food-delivery/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "1dec5a10cd0f58bcddc7",
+    "url": "/EPAM-food-delivery/static/css/main.8dd75111.chunk.css"
+  },
+  {
+    "revision": "8d49be821a6138055073",
+    "url": "/EPAM-food-delivery/static/js/2.80bf1ac1.chunk.js"
+  },
+  {
+    "revision": "86c8af751af5a3089a0acdd73d39fe96",
+    "url": "/EPAM-food-delivery/static/js/2.80bf1ac1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dec5a10cd0f58bcddc7",
+    "url": "/EPAM-food-delivery/static/js/main.c4bdbc9a.chunk.js"
+  },
+  {
+    "revision": "6121cea2f41c4c8ee1c2",
+    "url": "/EPAM-food-delivery/static/js/runtime-main.27923665.js"
+  },
+  {
+    "revision": "706af0ca4204db7a83901b2560ceb935",
+    "url": "/EPAM-food-delivery/static/media/back.706af0ca.svg"
+  },
+  {
+    "revision": "a49f1a90a986a18afa085771d9ebb147",
+    "url": "/EPAM-food-delivery/static/media/basket.a49f1a90.svg"
+  },
+  {
+    "revision": "69c3ab2cf2a541adb3c841bb12bf1e9e",
+    "url": "/EPAM-food-delivery/static/media/deliverman.69c3ab2c.png"
+  },
+  {
+    "revision": "d3d5088f6491df12be40d840fcc12e2d",
+    "url": "/EPAM-food-delivery/static/media/facebook.d3d5088f.svg"
+  },
+  {
+    "revision": "5745f2903a70efc766d6eb2ef25a8ea6",
+    "url": "/EPAM-food-delivery/static/media/georgia.5745f290.svg"
+  },
+  {
+    "revision": "aece8e0d0675b31ee5d2aebb42553b11",
+    "url": "/EPAM-food-delivery/static/media/georgia.aece8e0d.jpg"
+  },
+  {
+    "revision": "09adc1572d24047399a9a785a1d94e12",
+    "url": "/EPAM-food-delivery/static/media/georgian-cuisine(1).09adc157.jpg"
+  },
+  {
+    "revision": "692f2a16c164150762fb1f931fbed6a2",
+    "url": "/EPAM-food-delivery/static/media/georgian-cuisine.692f2a16.jpg"
+  },
+  {
+    "revision": "12592b3a22047e18cd35cb8902a82076",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-black.12592b3a.woff"
+  },
+  {
+    "revision": "226d519086a3835e16c87f4ab1cd5641",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-black.226d5190.woff2"
+  },
+  {
+    "revision": "37c37df55859d300a0bc06ebab70beda",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-black.37c37df5.ttf"
+  },
+  {
+    "revision": "3f9c5a2b33fd28799ac8e55c093a4531",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-bold.3f9c5a2b.otf"
+  },
+  {
+    "revision": "ad1867c1adc22645240b3fd33a205f9e",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-bold.ad1867c1.woff2"
+  },
+  {
+    "revision": "bfece6d8c1ec4da6086bc2ace36d5ba9",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-bold.bfece6d8.woff"
+  },
+  {
+    "revision": "792e5785377b31db37697448eba44281",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-light.792e5785.ttf"
+  },
+  {
+    "revision": "964e1508f92be294b2d5be7e8ef64192",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-light.964e1508.woff2"
+  },
+  {
+    "revision": "a28799775b388e696c2b160f08596c24",
+    "url": "/EPAM-food-delivery/static/media/gotham-pro-light.a2879977.woff"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/EPAM-food-delivery/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "8e114f3304c6e33481a052a4b8bea20e",
+    "url": "/EPAM-food-delivery/static/media/italian-cuisine(1).8e114f33.jpg"
+  },
+  {
+    "revision": "1831449003866d77dffe721d6bc5a13e",
+    "url": "/EPAM-food-delivery/static/media/italian-cuisine.18314490.jpg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/EPAM-food-delivery/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/EPAM-food-delivery/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "f63b0f8e0e9a68f095f853aec3787237",
+    "url": "/EPAM-food-delivery/static/media/japan.f63b0f8e.png"
+  },
+  {
+    "revision": "0067570bf3e93e5671d716777b6aab0c",
+    "url": "/EPAM-food-delivery/static/media/japanese-cuisine(1).0067570b.jpeg"
+  },
+  {
+    "revision": "cb8acb1fa693f2bb8e86cfe2ba875112",
+    "url": "/EPAM-food-delivery/static/media/japanese-cuisine.cb8acb1f.jpeg"
+  },
+  {
+    "revision": "88b5b0d4e90dc0ea6d6f08272cc110e8",
+    "url": "/EPAM-food-delivery/static/media/logo.88b5b0d4.png"
+  },
+  {
+    "revision": "7905cd48f705096fe6959893d1b447bd",
+    "url": "/EPAM-food-delivery/static/media/logout.7905cd48.svg"
+  },
+  {
+    "revision": "79e04f35e7711bcf1458b42f34cc46e7",
+    "url": "/EPAM-food-delivery/static/media/pizza-cuisine(1).79e04f35.jpg"
+  },
+  {
+    "revision": "75f43b5bf7cbae9f9ff439bf1646e14e",
+    "url": "/EPAM-food-delivery/static/media/pizza-cuisine.75f43b5b.jpg"
+  },
+  {
+    "revision": "5b452c17dc36aecd3ddb0d3d103d3e9a",
+    "url": "/EPAM-food-delivery/static/media/pizza.5b452c17.jpg"
+  },
+  {
+    "revision": "a217525e28c6a0cee328799883354b7f",
+    "url": "/EPAM-food-delivery/static/media/pizza.a217525e.svg"
+  },
+  {
+    "revision": "a451f456eac79406086651dc449c331a",
+    "url": "/EPAM-food-delivery/static/media/telegram.a451f456.svg"
+  },
+  {
+    "revision": "61d4aa8ac948266efa6b84c015d39614",
+    "url": "/EPAM-food-delivery/static/media/ukraine.61d4aa8a.jpg"
+  },
+  {
+    "revision": "ce25837339826640e4c91b0d81eecac5",
+    "url": "/EPAM-food-delivery/static/media/ukraine.ce258373.svg"
+  },
+  {
+    "revision": "4c7d9e2ef943d2a462ed9db83cf54d84",
+    "url": "/EPAM-food-delivery/static/media/ukrainian-cuisine(1).4c7d9e2e.jpg"
+  },
+  {
+    "revision": "624f9450687542bb0b6bd1a8130d3e6d",
+    "url": "/EPAM-food-delivery/static/media/ukrainian-cuisine.624f9450.jpg"
+  }
+]);
